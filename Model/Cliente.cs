9320f5afcc68e231e1643d3f1557c2e2d4a238d4
Rafael Stein <rafael.stein@hotmail.com.br>
2023-02-12 18:30:45 -0300

    Mudança nos dados de Endereço
    Removido o model, controller e view referente a:
    - Endereco
    Adicionados os campos referente a endereço em:
    - Cliente.cs
    - Fornecedor.cs
    Criação do controller:
    - ClienteController.cs

--- Model/Cliente.cs
+++ Model/Cliente.cs
@@ -5,9 +5,13 @@
   {
 
     public int ID { get; set; }
-    public int EnderecoID { get; set; }
     public string RazaoSocial_Nome { get; set; }
     public string CNPJ_CPF { get; set; }
+    public string Logradouro { get; set; }
+    public string Numero { get; set; }
+    public string Cidade { get; set; }
+    public string CEP { get; set; }
+    public string UF { get; set; }
     public string ContatoNome { get; set; }
     public string ContatoEmail { get; set; }
     public string ContatoTelefone { get; set; }
--- Model/Fornecedor.cs
+++ Model/Fornecedor.cs
@@ -8,6 +8,11 @@
     public int EnderecoID { get; set; }
     public string RazaoSocial { get; set; }
     public string CNPJ { get; set; }
+    public string Logradouro { get; set; }
+    public string Numero { get; set; }
+    public string Cidade { get; set; }
+    public string CEP { get; set; }
+    public string UF { get; set; }
     public string ContatoNome { get; set; }
     public string ContatoEmail { get; set; }
     public string ContatoTelefone { get; set; }
